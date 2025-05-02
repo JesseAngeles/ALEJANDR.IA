6845a35e45ed9b8c519594ba6881fbb85f08de33
luismXD <mayito04rw@gmail.com>
2025-05-02 14:07:41 -0600

    Update Book.ts

--- backend/src/Routes/Book.ts
+++ backend/src/Routes/Book.ts
@@ -1,35 +1,13 @@
-// import { Router } from 'express'
-// import { createBook, getBooks, getBookById, updateBook, deleteBook, getRecommendedBooks } from '../Controllers/Book'
-// import { authenticateToken, authorizeRole } from '../Middleware/jwt'
-// import { validateRequest } from '../Middleware/ValidateRequest'
-// import { ValBookSchema } from '../Validations/Book'
-// import { createComment, deleteComment } from '../Controllers/Review'
-
-// const routerBook = Router()
-
-// // CRUD (title, author, price, ISBN, stock, image)
-// routerBook.post('/', authenticateToken, authorizeRole("admin"), validateRequest(ValBookSchema), createBook)
-// routerBook.get('/', getBooks)
-// routerBook.get('/recommended', getRecommendedBooks)
-// routerBook.get('/:ISBN', getBookById)  
-// routerBook.put('/:ISBN', authenticateToken, authorizeRole("admin"), validateRequest(ValBookSchema), updateBook)
-// routerBook.delete('/:ISBN', authenticateToken, authorizeRole("admin"), deleteBook)
-
-// // OPINIONES (comment, rating)
-// routerBook.post('/opinion/:ISBN', authenticateToken, authorizeRole("user"), createComment)
-// routerBook.delete('/opinion/:ISBN/:reviewId', authenticateToken, deleteComment)
-
-
-
-import { Router } from 'express';
-import { createBook, getBooks, getBookById, updateBook, deleteBook, updateReview, addReview, deleteReview, getRecommendedBooks} from '../Controllers/BookController';
+import { Router } from 'express'
+import { createBook, getBooks, getBookById, updateBook, deleteBook, getRecommendedBooks } from '../Controllers/Book'
 import { authenticateToken, authorizeRole } from '../Middleware/jwt'
 import { validateRequest } from '../Middleware/ValidateRequest'
 import { ValBookSchema } from '../Validations/Book'
 import { createComment, deleteComment } from '../Controllers/Review'
-const router = Router();
 
+const routerBook = Router()
 
+// CRUD (title, author, price, ISBN, stock, image)
 routerBook.post('/', authenticateToken, authorizeRole("admin"), validateRequest(ValBookSchema), createBook)
 routerBook.get('/', getBooks)
 routerBook.get('/recommended', getRecommendedBooks)
@@ -39,9 +17,31 @@ routerBook.delete('/:ISBN', authenticateToken, authorizeRole("admin"), deleteBoo
 
 // OPINIONES (comment, rating)
 routerBook.post('/opinion/:ISBN', authenticateToken, authorizeRole("user"), createComment)
-routerBook.put('/:ISBN/reviews/:userId', updateReview);
 routerBook.delete('/opinion/:ISBN/:reviewId', authenticateToken, deleteComment)
-// routerBook.delete('/opinion/:ISBN/:userid', authenticateToken, deleteComment)
+
+
+
+// import { Router } from 'express';
+// import { createBook, getBooks, getBookById, updateBook, deleteBook, updateReview, addReview, deleteReview, getRecommendedBooks} from '../Controllers/BookController';
+// import { authenticateToken, authorizeRole } from '../Middleware/jwt'
+// import { validateRequest } from '../Middleware/ValidateRequest'
+// import { ValBookSchema } from '../Validations/Book'
+// import { createComment, deleteComment } from '../Controllers/Review'
+// const router = Router();
+
+
+// routerBook.post('/', authenticateToken, authorizeRole("admin"), validateRequest(ValBookSchema), createBook)
+// routerBook.get('/', getBooks)
+// routerBook.get('/recommended', getRecommendedBooks)
+// routerBook.get('/:ISBN', getBookById)  
+// routerBook.put('/:ISBN', authenticateToken, authorizeRole("admin"), validateRequest(ValBookSchema), updateBook)
+// routerBook.delete('/:ISBN', authenticateToken, authorizeRole("admin"), deleteBook)
+
+// // OPINIONES (comment, rating)
+// routerBook.post('/opinion/:ISBN', authenticateToken, authorizeRole("user"), createComment)
+// routerBook.put('/:ISBN/reviews/:userId', updateReview);
+// routerBook.delete('/opinion/:ISBN/:reviewId', authenticateToken, deleteComment)
+// // routerBook.delete('/opinion/:ISBN/:userid', authenticateToken, deleteComment)
 
 
 export default routerBook
